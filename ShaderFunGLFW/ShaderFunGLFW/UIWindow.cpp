0eac9da45b0132a56a08dea482a19f4fa2bc9185
Jack Armstrong <mackycheese21@gmail.com>
2018-06-24 18:54:24 -0700

    No more broken ShaderFunGLFW

--- ShaderFunGLFW/ShaderFunGLFW/UIWindow.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  UIWindow.cpp
-//  ShaderFunGLFW
-//
-//  Created by Jack Armstrong on 5/20/18.
-//  Copyright © 2018 Jack Armstrong. All rights reserved.
-//
-
-#include "UIWindow.hpp"
--- ShaderFunGLFW/ShaderFunGLFW/shader.vert
+++
@@ -1,8 +0,0 @@
-
-#version 330 core
-layout (location = 0) in vec3 aPos;
-//out vec4 outPos;
-//uniform mat4x4 ViewMatrix;
-void main() {
-    gl_Position=vec4(aPos.xyz,1.0);
-}
